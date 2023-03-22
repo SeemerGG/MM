14431a7b3ed8a8af14081766a426c23ed5f1be0f
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-03-22 12:26:45 +0300

    Промежуточный результат

---
+++ RungeKut/RungeKut/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace RungeKut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(13, 13);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(774, 357);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество точек";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 377);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(774, 59);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(383, 24);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Пример №2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(274, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Пример №1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RungeKut/RungeKut/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace RungeKut
+{
+    using func2 = Func<double, double, double>;
+    using func1 = Func<double, double>;
+
+    public partial class Form1 : Form
+    {
+
+        static double eps = 0.001;
+
+        (func2 x, func2 y, func1 x_s, func1 y_s, double x0, double y0) get_test1()
+        {
+            double f_x(double x, double y)
+            {
+                return -2 * x + 4 * y;
+            }
+
+            double f_y(double x, double y)
+            {
+                return -x + 3 * y;
+            }
+
+            double f_x_s(double t)
+            {
+                return 4 * Math.Exp(-t) - Math.Exp(2 * t);
+            }
+
+            double f_y_s(double t)
+            {
+                return Math.Exp(-t) - Math.Exp(2 * t);
+            }
+
+            return (f_x, f_y, f_x_s, f_y_s, 3, 0);
+        }
+
+        (func2 x, func2 y, func1 x_s, func1 y_s, double x0, double y0) get_test2()
+        {
+            double f_x(double x, double y)
+            {
+                return y;
+            }
+
+            double f_y(double x, double y)
+            {
+                return 2 * y;
+            }
+
+            double f_x_s(double t)
+            {
+                return Math.Exp(2 * t) + 1;
+            }
+
+            double f_y_s(double t)
+            {
+                return 2 * Math.Exp(2 * t);
+            }
+
+            return (f_x, f_y, f_x_s, f_y_s, 2, 2);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void SolveAndShow(int n, bool flag)
+        {
+            func2 f_x, f_y;
+            func1 x_s, y_s;
+            double x0, y0;
+            if(flag)
+            {
+                (f_x, f_y, x_s, y_s, x0, y0) = get_test1();
+            }
+            else
+            {
+                (f_x, f_y, x_s, y_s, x0, y0) = get_test1();
+            }
+            
+            Range r = new Range(0, 1);
+
+            PointPairList s = MethodRungeKuti.Solve2EquationSystem(f_x, f_y, r, n, x0, y0);
+
+            List<double> x = new List<double>(), y = new List<double>();
+
+            for (double t = r.Start; t <= r.End; t += eps)
+            {
+                x.Add(x_s(t));
+                y.Add(y_s(t));
+            }
+
+            PointPairList points = new PointPairList(x.ToArray(), y.ToArray());
+
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            pane.Title.Text = "Метод Рунге-Куты";
+
+            LineItem line1 = pane.AddCurve("Точное решение", points, Color.BlueViolet, SymbolType.None);
+            LineItem line2 = pane.AddCurve("Приблеженное решение", s, Color.Green, SymbolType.None);
+
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+        //private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        //{
+            
+        //    if(radioButton1.Checked)
+        //    {
+        //        SolveAndShow(n, true);
+        //    }
+        //    if(radioButton2.Checked)
+        //    {
+        //        SolveAndShow(n, false);
+        //    }
+        //}
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            zedGraphControl1.Controls.Clear();
+            int n = Convert.ToInt32(textBox1.Text);
+            SolveAndShow(n, true);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            zedGraphControl1.Controls.Clear();
+            int n = Convert.ToInt32(textBox1.Text);
+            SolveAndShow(n, false);
+        }
+    }
+}
